b65bfd481d34b55ecff690c42c4f0e6c359bc385
Christopher Dietz <25520174+chris-dietz@users.noreply.github.com>
2022-04-19 13:58:37 -0600

    Added page components to seperate file

---
+++ ddns-web-client/src/Page.js
@@ -0,0 +1,12 @@
+import Header from "./components/header/Header"
+import Footer from "./components/Footer"
+import Body from "./components/Body"
+export default function Page(props){
+    return(
+        <>
+            <Header></Header>
+            <Body></Body>
+            <Footer></Footer>
+        </>
+    )
+}
