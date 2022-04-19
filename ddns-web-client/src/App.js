57f0f55efff0cc75708b8406a85c3c3db439adae
Christopher Dietz <25520174+chris-dietz@users.noreply.github.com>
2022-04-19 13:58:47 -0600

    Updated app to use page

--- ddns-web-client/src/App.js
+++ ddns-web-client/src/App.js
@@ -1,32 +1,12 @@
 
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Web3 from 'web3';
-import Header from './components/header/Header'
+import Page from './Page';
 
 function App() {
 
-  const [account, setAccount] = useState(); // state variable to set account.
-  
-  useEffect(() => {
-    async function load() {
-      const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-      const accounts = await web3.eth.requestAccounts();
-      
-      setAccount(accounts[0]);
-    }
-    
-    load();
-   }, []);
   return (
-    <div>
-      <Header></Header>
-      <div>
-        Your account is: {account}
-      </div>
-    </div>
+    <Page></Page>
   );
 }
 
