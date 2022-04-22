74a2676787c07543375221107da6ce3a28933dc9
tjhamlet <tyler.craig@colostate.edu>
2022-04-22 04:18:47 +0000

    Add toggling Menu button

--- ddns-web-client/src/components/header/Navigation.js
+++ ddns-web-client/src/components/header/Navigation.js
@@ -1,26 +1,34 @@
-import React, {useState} from "react";
-import {Nav,Collapse, Navbar, NavbarBrand,NavItem,NavLink} from "reactstrap";
+import React, { useState } from "react";
+import { Nav, Collapse, Navbar, NavbarBrand, NavItem, NavLink, Button } from "reactstrap";
 
-export default function NavigationBar(props){
-    return(   
-    <Navbar color="light" expand="md" light>
-        <NavbarBrand >
+export default function NavigationBar(props) {
+   const [navbarOpen, setNavbarOpen] = useState(false)
+   return (
+      <Navbar color="light" expand="md" light>
+         <NavbarBrand>
+            <Button onClick={() => setNavbarOpen(!navbarOpen)}>
+               {/* <img src="dotted-arrow.svg" class="filter-green" /> */}
+               <svg fill="#353535" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path d="M0 96C0 78.33 14.33 64 32 64H416C433.7 64 448 78.33 448 96C448 113.7 433.7 128 416 128H32C14.33 128 
+                        0 113.7 0 96zM0 256C0 238.3 14.33 224 32 224H416C433.7 224 448 238.3 448 256C448 273.7 433.7 288 416 288H32C14.33 288 
+                        0 273.7 0 256zM416 448H32C14.33 448 0 433.7 0 416C0 398.3 14.33 384 32 384H416C433.7 384 448 398.3 448 416C448 433.7 
+                        433.7 448 416 448z" /></svg>
+            </Button>
             CSU Decentalized Domain Registrar
-        </NavbarBrand >
-        <Collapse navbar>
+         </NavbarBrand >
+         <Collapse isOpen={navbarOpen} navbar>
             <Nav className="me-auto" navbar>
-                <NavItem>
-                    <NavLink href="/index/">Register a Domain</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/manage/">Manage Domains</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/lookup/">Lookup Domains</NavLink>
-                </NavItem>
+               <NavItem>
+                  <NavLink href="/index/">Register a Domain</NavLink>
+               </NavItem>
+               <NavItem>
+                  <NavLink href="/manage/">Manage Domains</NavLink>
+               </NavItem>
+               <NavItem>
+                  <NavLink href="/lookup/">Lookup Domains</NavLink>
+               </NavItem>
             </Nav>
-        </Collapse>
-    </Navbar>
-    
-    )
+         </Collapse>
+      </Navbar>
+   )
 }
