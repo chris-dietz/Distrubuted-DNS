3c8f9131f0691ebbe2e7e967422cba2d5cc627a7
Christopher Dietz <25520174+chris-dietz@users.noreply.github.com>
2022-04-25 17:33:25 -0600

    Replaced web3 api call the ethers.js

--- ddns-web-client/src/components/Body.js
+++ ddns-web-client/src/components/Body.js
@@ -1,14 +1,13 @@
 
 import { useEffect, useState } from 'react';
-import Web3 from 'web3';
+import { ethers } from 'ethers'
 export default function Body(props) {
     const [account, setAccount] = useState(); // state variable to set account.
   
   useEffect(() => {
     async function load() {
-      const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-      const accounts = await web3.eth.requestAccounts();
-      
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const accounts = await provider.send("eth_requestAccounts")
       setAccount(accounts[0]);
     }
     
