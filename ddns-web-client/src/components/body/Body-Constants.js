198a94f995b2eba4e23cb2d8628df1e849f94573
Christopher Dietz <25520174+chris-dietz@users.noreply.github.com>
2022-04-25 22:52:36 -0600

    Added regex to validate domain names

--- ddns-web-client/src/components/body/Body-Constants.js
+++ ddns-web-client/src/components/body/Body-Constants.js
@@ -1,4 +1,4 @@
 export const searchPlaceholderText = "Register a domain to start"
 export const searchButtonText = "Register"
 export const testDomain = "alreadytaken.csu"
-export const registrationFormValidationRegex = /^[a-zA-Z0-9]+.csu$/
+export const registrationFormValidationRegex =new RegExp(/^[a-zA-Z0-9]+.csu$/)
