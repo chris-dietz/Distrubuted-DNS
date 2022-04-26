73a3d72e9669f03da332c6f21232fab753b3a5b6
Christopher Dietz <25520174+chris-dietz@users.noreply.github.com>
2022-04-25 22:52:44 -0600

    Added form validation

--- ddns-web-client/src/components/body/Search.js
+++ ddns-web-client/src/components/body/Search.js
@@ -1,16 +1,18 @@
 import {Input,Button} from "reactstrap"
 import ResultsPage from "./ResultsPage"
 import { useState } from "react"
+import { searchButtonText , testDomain, registrationFormValidationRegex} from "./Body-Constants"
     
 export default function Search(props){
     const [showResults, setShowResults] = useState(false)
     const [query,setQuery] = useState("")
     const [domainAvailable,setDomainAvaliable] = useState(false)
+    const [invalidInput,setInvalidInput] = useState(false)
     
     return (
     <div>
-        <Input type="search" placeholder={props.placeholder} onChange={(e) =>handleUserTyping(e.target.value)}></Input>
-        <Button onClick={handleButtonClick}>Search</Button>
+        <Input invalid={invalidInput} type="search" placeholder={props.placeholder} onChange={(e) =>handleUserTyping(e.target.value)}></Input>
+        <Button onClick={handleButtonClick}>{searchButtonText}</Button>
         { showResults ? <ResultsPage query={query} isDomainAvailable={domainAvailable} /> : null }
     </div>
     )
@@ -18,16 +20,28 @@ export default function Search(props){
     function handleUserTyping(query){
         setQuery(query)
         setShowResults(false)
+        setInvalidInput(false)
     }
 
     function handleButtonClick(){
-        setShowResults(true)
-        setDomainAvaliable(isDomainAvailable(query))
+        if(validateInput(query)){
+            setShowResults(true)
+            setDomainAvaliable(isDomainAvailable(query))
+        }
+        else{
+            setInvalidInput(true)
+        }
+
     }
+}
+
+
+function validateInput(query){
+    return registrationFormValidationRegex.test(query)
 }
 
 //Function will eventually call the etherieum contract to determine if a domain is available
 //For now just return true unless it uses the test domain alreadytaken.csu
 function isDomainAvailable(queryString){
-    return queryString !== "alreadytaken.csu"
+    return queryString !== testDomain
 }
