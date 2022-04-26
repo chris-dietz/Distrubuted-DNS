266fc01387f4323131913dbd014d3189b2dd3e89
Christopher Dietz <25520174+chris-dietz@users.noreply.github.com>
2022-04-25 23:12:08 -0600

    Added error message constant

--- ddns-web-client/src/components/body/Body-Constants.js
+++ ddns-web-client/src/components/body/Body-Constants.js
@@ -2,3 +2,4 @@ export const searchPlaceholderText = "Register a domain to start"
 export const searchButtonText = "Register"
 export const testDomain = "alreadytaken.csu"
 export const registrationFormValidationRegex =new RegExp(/^[a-zA-Z0-9]+.csu$/)
+export const invalidDomainErrorMessage = "Invalid Domain! (Domains must end in .csu)"
