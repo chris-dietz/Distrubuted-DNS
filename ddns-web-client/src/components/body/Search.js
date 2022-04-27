46c8bf1636308322818ed6b93f52c528e2324b94
tjhamlet <tyler.craig@colostate.edu>
2022-04-27 18:58:15 +0000

    Formatting

--- ddns-web-client/src/components/body/Search.js
+++ ddns-web-client/src/components/body/Search.js
@@ -1,44 +1,50 @@
-import {Input,Button,Form, FormFeedback, FormGroup} from "reactstrap"
+import { Input, Button, Form, FormFeedback, FormGroup, Row, Col } from "reactstrap"
 import ResultsPage from "./ResultsPage"
 import { useState } from "react"
-import { searchButtonText , testDomain, registrationFormValidationRegex,invalidDomainErrorMessage} from "./Body-Constants"
-    
-export default function Search(props){
+import { searchButtonText, testDomain, registrationFormValidationRegex, invalidDomainErrorMessage } from "./Body-Constants"
+
+export default function Search(props) {
     const [showResults, setShowResults] = useState(false)
-    const [query,setQuery] = useState("")
-    const [domainAvailable,setDomainAvaliable] = useState(false)
-    const [invalidInput,setInvalidInput] = useState(false)
-    
+    const [query, setQuery] = useState("")
+    const [domainAvailable, setDomainAvaliable] = useState(false)
+    const [invalidInput, setInvalidInput] = useState(false)
+
     return (
-    <div>
-        <Form onSubmit={(e) => handleSearch(e)}>
-            <FormGroup>
-                <Input invalid={invalidInput} type="search" placeholder={props.placeholder} onChange={(e) =>handleUserTyping(e.target.value)}></Input>
-                <FormFeedback invalid="true" >{invalidDomainErrorMessage}</FormFeedback>
-            </FormGroup>
-            <Button type="submit">{searchButtonText}</Button>
-        </Form>
-        { showResults ? <ResultsPage query={query} isDomainAvailable={domainAvailable} /> : null }
-    </div>
+        <div>
+            <Form onSubmit={(e) => handleSearch(e)}>
+                <Row>
+                    <Col>
+                        <FormGroup>
+                            <Input invalid={invalidInput} type="search" placeholder={props.placeholder} onChange={(e) => handleUserTyping(e.target.value)}></Input>
+                            <FormFeedback invalid="true" >{invalidDomainErrorMessage}</FormFeedback>
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <Button type="submit">{searchButtonText}</Button>
+                    </Col>
+                </Row>
+            </Form>
+            {showResults ? <ResultsPage query={query} isDomainAvailable={domainAvailable} /> : null}
+        </div>
     )
 
-    function handleUserTyping(query){
+    function handleUserTyping(query) {
         setQuery(query)
-        if(showResults){
+        if (showResults) {
             setShowResults(false)
         }
-        if(invalidInput){
+        if (invalidInput) {
             setInvalidInput(false)
         }
     }
 
-    function handleSearch(e){
+    function handleSearch(e) {
         e.preventDefault()
-        if(validateInput(query)){
+        if (validateInput(query)) {
             setShowResults(true)
             setDomainAvaliable(isDomainAvailable(query))
         }
-        else{
+        else {
             setInvalidInput(true)
         }
 
@@ -46,12 +52,12 @@ export default function Search(props){
 }
 
 
-function validateInput(query){
+function validateInput(query) {
     return registrationFormValidationRegex.test(query)
 }
 
 //Function will eventually call the etherieum contract to determine if a domain is available
 //For now just return true unless it uses the test domain alreadytaken.csu
-function isDomainAvailable(queryString){
+function isDomainAvailable(queryString) {
     return queryString !== testDomain
 }
