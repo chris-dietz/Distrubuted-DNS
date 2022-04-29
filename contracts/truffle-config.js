cf0bbf32b48b01438880d3e101ba02e6968951a0
Christopher Dietz <25520174+chris-dietz@users.noreply.github.com>
2022-04-29 15:51:41 -0600

    Changed config to output abis to web app directory

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -46,6 +46,12 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+     },
+
+     wsl: {
+        host: "172.23.160.1",     // Localhost (default: none)
+        port: 8545,            // Standard Ethereum port (default: none)
+        network_id: "*",       // Any network (default: none)
      }
     // Another network with more advanced options...
     // advanced: {
