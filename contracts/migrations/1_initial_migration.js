55234ffb4587c33e0a729c4abf494e08235c69c5
Christopher Dietz <25520174+chris-dietz@users.noreply.github.com>
2022-04-29 14:56:56 -0600

    Added code for contract deployment

--- contracts/migrations/1_initial_migration.js
+++ contracts/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const BlockChainDNS = artifacts.require("BlockchainDNS")
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(BlockChainDNS)
 };
